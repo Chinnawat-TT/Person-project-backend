9acb6ab28d28a339eecc3e655c275862025954b5
Chinnawat-TT <Chinnawat.sangrung@gmail.com>
2023-10-10 11:13:12 +0700

    setup project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require('morgan')
+const cors = require("cors");
+
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error')
+const ratelimitMiddleware = require('./middleware/rate-limit')
+
+const app = express();
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(ratelimitMiddleware)
+app.use(express.json())
+
+
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+const PORT =process.env.PORT || '5000'
+app.listen(PORT ,()=> console.log(`server run on port : ${PORT}`))
